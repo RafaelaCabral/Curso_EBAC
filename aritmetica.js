a81fd9246f6fe1510b9da45068fabd684d946323
Rafaela Vieira Cabral <rafaelavieiracabral04@gmail.com>
2025-06-25 21:10:10 -0300

    feat: implementação do gulp para compilação do sass

--- aritmetica.js
+++
@@ -1,3 +0,0 @@
-const somar = require('./sum.js');
-
-console.log('Somando 2 + 3 =', somar(2, 3));
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
 
+function compilaSass() {
+    return gulp.src("./source/styles/main.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: "compressed"
+        }))
+        .pipe(sourcemaps.write("./maps"))
+        .pipe(gulp.dest("./build/styles"));
+
+}
 function funcaoPadrao(callback) {
     setTimeout(function () {
         console.log("Executando via Gulp");
@@ -20,3 +32,8 @@ function dizTchau() {
     console.log("Tchau, Gulp!");
 }
 exports.default = gulp.parallel(funcaoPadrao, dizOi);
+exports.sass = compilaSass;
+exports.watch = function () {
+    gulp.watch("./source/styles/*.scss", {ignoreInitial: false}, gulp.series(compilaSass));
+
+}
